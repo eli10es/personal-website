c9e03d6c37972ebe16df40326ededceb24a3010f
Elisei <eli10es@yahoo.es>
2025-09-24 20:43:58 +0300

    header added

---
+++ personal-website/src/app/about-contact/about-contact.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-contact',
+  imports: [],
+  templateUrl: './about-contact.component.html',
+  styleUrl: './about-contact.component.css'
+})
+export class AboutContactComponent {
+
+}
---
+++ personal-website/src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+export const routes: Routes = [{ path: '', component: HomePageComponent }];
